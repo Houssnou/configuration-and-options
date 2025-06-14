d7977fea0f66a145c0b6ad4fe2ad0be8e271d0dd
Houssnou <hosni.moubarak@gmail.com>
2025-06-14 17:01:05 -0400

    add class to map to logging setting from app settings and settings binding

---
+++ ConfigurationsWithOptionsPattern/Logging/LoggingOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace ConfigurationsWithOptionsPattern.Logging
+{
+    public class LoggingOptions
+    {
+        public const string LoggingConfigurationSectionName = "Logging";
+
+        public LogLevelOptions? LogLevel { get; set; }
+    }
+
+    public sealed class LogLevelOptions
+    : Dictionary<string, LogLevel>
+    {
+        /// <inheritdoc cref="Dictionary{TKey, TValue}" />
+        public LogLevelOptions()
+            : base(StringComparer.OrdinalIgnoreCase)
+        {
+        }
+    }
+}
