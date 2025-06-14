8eb90cabf2001abd3b634281c4e3a1cac197a595
Houssnou <hosni.moubarak@gmail.com>
2025-06-14 19:15:15 -0400

    added differents binding approaches

---
+++ ConfigurationsWithOptionsPattern/Feature/FeatureOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace ConfigurationsWithOptionsPattern.Feature
+{
+    public sealed class FeatureOptions
+    {    
+        public bool Enabled { get; set; }
+
+        public string? Name { get; set; }
+
+        public Version? Version { get; set; }
+
+        public Uri? Endpoint { get; set; }
+
+        public string? ApiKey { get; set; }
+
+        public string[] Tags { get; set; } = [];
+    }
+}
--- ConfigurationsWithOptionsPattern/Program.cs
+++ ConfigurationsWithOptionsPattern/Program.cs
@@ -1,46 +1,82 @@
+using ConfigurationsWithOptionsPattern.Feature;
 using ConfigurationsWithOptionsPattern.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOptions<LoggingOptions>()
     .Bind(config: builder.Configuration.GetSection(key: LoggingOptions.LoggingConfigurationSectionName));
 
+builder.Services.AddOptions<FeatureOptions>(name: "TodoApi").Bind(config: builder.Configuration.GetSection(key: "Features:TodoApi"));
+
+//builder.Services.AddOptions<FeatureOptions>(name: "WeatherStation").Bind(config: builder.Configuration.GetSection(key: "Features:WeatherStation"));
+
+// overrides and/or merges with existing configured bindings
+builder.Services.PostConfigure<FeatureOptions>(
+    name: "WeatherStation",
+    configureOptions: static (FeatureOptions options) =>
+    {
+        options.Version = new(1, 0);
+        options.Endpoint = new(uriString: "https://freetestapi.com/api/v1/weathers");
+        options.Tags = [
+            "fake-weather",
+            "test-api"
+            ];
+    });
+
+// override all config bound instances of FeatureOptions
+builder.Services.PostConfigureAll<FeatureOptions>(
+    configureOptions: static (FeatureOptions options) => options.Tags ??= []
+    );
+
+builder.Services.Configure<FeatureOptions>(
+    name: "WeatherStation",
+    config: builder.Configuration.GetSection(key: "Features:WeatherStation"),
+    configureBinder: static (BinderOptions o) =>
+    {
+        o.BindNonPublicProperties = true;
+        o.ErrorOnUnknownConfiguration = false;
+    });
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet(pattern: "/logging/options", handler: static (IOptions<LoggingOptions> options) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var loggingOptions = options.Value;
+    return Results.Json(statusCode: StatusCodes.Status200OK, data: loggingOptions);
 })
-.WithName("GetWeatherForecast");
+.WithName("GetLoggingOptions")
+.WithOpenApi();
+
+
+
+app.MapGet(pattern: "/features", handler: static (IOptionsSnapshot<FeatureOptions> options) =>
+{
+    var todoOptions = options.Get(name: "TodoApi");
+    var weatherOptions = options.Get(name: "WeatherStation");
+    return Results.Json(statusCode: StatusCodes.Status200OK, data: new
+    {
+        TodoApi = todoOptions,
+        WeatherStation = weatherOptions
+    });
+})
+.WithName("GetFeaturesOptions")
+.WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
